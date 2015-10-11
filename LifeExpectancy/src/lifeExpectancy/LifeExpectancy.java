010d563e06bbd8ed8e6ed32dc8dc5f35033a26d6
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-10-11 14:44:16 +0100

    Life Expectancy

--- LifeExpectancy/src/lifeExpectancy/LifeExpectancy.java
+++ LifeExpectancy/src/lifeExpectancy/LifeExpectancy.java
@@ -3,6 +3,8 @@
  */
 package lifeExpectancy;
 
+import java.util.Map;
+
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
@@ -14,12 +16,16 @@ import processing.core.PApplet;
  */
 public class LifeExpectancy extends PApplet {
 	UnfoldingMap map;
+	Map<String, Float> lifeExpByCountry; 
 
     public void setup() {
     	size(800, 600, OPENGL);
     	map = new UnfoldingMap(this, 50, 50, 700, 500, 
     			new Google.GoogleMapProvider());
     	MapUtils.createDefaultEventDispatcher(this, map);
+    	
+    	lifeExpByCountry = loadLifeExpectancyFromCSV
+    			("data/LifeExpectancyWorldBank.csv");
     }
 
     public void draw() {
