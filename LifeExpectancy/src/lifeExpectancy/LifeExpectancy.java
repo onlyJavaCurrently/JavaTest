ef8fb5b77302e2f7e5e926653eeee410ae8720f0
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-10-11 15:26:49 +0100

    Life Expectancy

--- LifeExpectancy/src/lifeExpectancy/LifeExpectancy.java
+++ LifeExpectancy/src/lifeExpectancy/LifeExpectancy.java
@@ -4,9 +4,13 @@
 package lifeExpectancy;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
@@ -17,12 +21,23 @@ import processing.core.PApplet;
  */
 public class LifeExpectancy extends PApplet {
 	UnfoldingMap map;
-	Map<String, Float> lifeExpByCountry; 
+	Map<String, Float> lifeExpByCountry;
+	
+	List<Feature> countries;
+	List<Marker> countryMarkers;
 
 	private Map<String, Float> loadLifeExpectancyFromCSV(String fileName){
 		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
 		
 		String[] rows = loadStrings(fileName);
+		
+		for(String row: rows) {
+			String[] columns = row.split(",");
+			if(columns.length > 0) {
+				float value = Float.parseFloat(columns[5]);
+				lifeExpMap.put(columns[4], value);
+			}
+		}
 		
 		return lifeExpMap;
 	}
@@ -35,6 +50,10 @@ public class LifeExpectancy extends PApplet {
     	
     	lifeExpByCountry = loadLifeExpectancyFromCSV
     			("data/LifeExpectancyWorldBank.csv");
+    	
+    	countries = GeoJSONReader.loadData(this, "data/countries.geo.json");
+    	countryMarkers = MapUtils.createSimpleMarkers(countries);
+
     }
 
     public void draw() {
