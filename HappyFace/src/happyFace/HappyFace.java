06ac637cfef00d468a2251aa4631bc0caed6f652
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-10-11 21:00:07 +0100

    Happy Face

--- HappyFace/src/happyFace/HappyFace.java
+++ HappyFace/src/happyFace/HappyFace.java
@@ -11,4 +11,12 @@ import processing.core.PApplet;
  */
 public class HappyFace extends PApplet{
 	
+	public void setup() {
+		size(500, 500);
+		background(150, 150, 150);
+	}
+	
+	public void draw() {
+		ellipse(50, 100, 75, 75);
+	}
 }
