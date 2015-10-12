52c7154510c3ceb86665c47dd9e2111daf94ac28
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-10-12 07:17:26 +0100

    Happy Face

--- HappyFace/src/happyFace/HappyFace.java
+++ HappyFace/src/happyFace/HappyFace.java
@@ -1,5 +1,7 @@
 /**
  * Painting happy faces
+ * Lib dependencies:
+ *     core.jar  (processing.core)
  */
 package happyFace;
 
@@ -10,19 +12,19 @@ import processing.core.PApplet;
  *
  */
 public class HappyFace extends PApplet{
-	
-	public void setup() {
-		size(500, 500);
-		background(150, 150, 150);
-	}
-	
-	public void draw() {
-		fill(40, 40, 200);
-		ellipse(250, 250, 220, 220);
-		fill(255, 255, 255);
-		ellipse(210, 210, 30, 25);
-		ellipse(290, 210, 30, 25);
-		fill(255, 40, 40);
-		arc(250, 275, 140, 110, 0, PI);
-	}
+    
+    public void setup() {
+        size(500, 500);
+        background(150, 150, 150);
+    }
+    
+    public void draw() {
+        fill(40, 40, 200);
+        ellipse(250, 250, 220, 220);
+        fill(255, 255, 255);
+        ellipse(210, 210, 30, 25);
+        ellipse(290, 210, 30, 25);
+        fill(255, 40, 40);
+        arc(250, 275, 140, 110, 0, PI);
+    }
 }
